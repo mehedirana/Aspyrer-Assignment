9b47bd526ebf25db6a4c2ca489634e3729b4d29f
mehedi rana <mdmehedirana@gmail.com>
2020-08-27 00:31:50 +0600

    app modified

--- App.js
+++ App.js
@@ -6,8 +6,17 @@ import { View, Text } from 'react-native';
 export default function App() {
 
   return (
- <View>
+ <View style={StyleSheet.contrainer}>
    <Text>HI</Text>
  </View>
   );
 }
+
+const styles = StyleSheet.create({
+   contrainer: {
+     flex: 1,
+     justyfyContent: "center",
+     alignItems: "center"
+
+   }
+});
