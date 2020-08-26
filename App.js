06fc40dc1de4cd955d4503d88f3d6dbe53a0795a
mehedi rana <mdmehedirana@gmail.com>
2020-08-27 00:28:22 +0600

    refresh

--- App.js
+++ App.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-import SignInScreen from './screens/SignInScreen';
-import SignUpScreen from './screens/SignUpScreen';
+import { View, Text } from 'react-native';
 
-const Stack = createStackNavigator();
 
 
 export default function App() {
+
   return (
-    <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
-      <Stack.Screen name="SignInScreen" component={SignInScreen} />
-    </Stack.Navigator>
-  </NavigationContainer>
+ <View>
+   <Text>HI</Text>
+ </View>
   );
 }
