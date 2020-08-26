f9a0d12b029228118d8d35f79c282728dab97a81
mehedi rana <mdmehedirana@gmail.com>
2020-08-26 18:26:47 +0600

    add two

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import SignInScreen from './screens/SignInScreen';
-import SignUpScreen from './screens/SignUpScreen';
+// import SignInScreen from './screens/SignInScreen';
+// import SignUpScreen from './screens/SignUpScreen';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-    <view>
-      <SignInScreen/>
-      <SignUpScreen/>
-    </view>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 const styles = StyleSheet.create({
   container: {
     flex: 1,
